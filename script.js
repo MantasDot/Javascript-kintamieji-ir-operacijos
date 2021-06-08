c1640e7c8c694152f578eacc8300e04d6099ec31
MANTAS_JUODELIS <MANtas.juodelis1@gmail.com>
2021-06-08 23:27:42 +0300

    PirmasND

---
+++ script.js
@@ -0,0 +1,42 @@
+"Use-strict";
+// Step 2-3.
+var skaicius1 = 4;
+var skaicius2 = 9;
+var skaicius3 = 15;
+var skaicius4 = 53;
+
+
+var suma = skaicius1 + skaicius2 + skaicius3 + skaicius4
+var sandauga = skaicius1 * skaicius2 * skaicius3 * skaicius4
+
+console.log(skaicius1);
+console.log(skaicius2);
+console.log(skaicius3);
+console.log(skaicius4);
+console.log(suma);
+console.log(sandauga);
+
+
+// Step 4.
+var temp = skaicius1
+skaicius1 = skaicius2
+skaicius2 = temp
+console.log(skaicius1,skaicius2);
+
+// Step 5.
+
+skaicius3 = skaicius3 + skaicius4;
+skaicius4 = skaicius3 - skaicius4;
+skaicius3 = skaicius3 - skaicius4;
+console.log(skaicius3,skaicius4)
+
+// Step 6.
+alert ('Skaicius1 =' + skaicius1 + '; Skaicius2 =' + skaicius2 + '; Skaicius3 =' + skaicius3  + '; Skaicius4 =' + skaicius4 + ';')
+
+
+// Step 7.
+var maxSkaicius = Number.MAX_VALUE
+console.log(maxSkaicius)
+
+// 
+
